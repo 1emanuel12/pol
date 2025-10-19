d346364d1a8384ac293070105e23f010d080b4da
1emanuel12 <1emanuel3030@gmail.com>
2025-10-19 08:52:26 -0500

    LevelUpMessage.js
    Removed redundant processing for object values and cleaned up message assignment logic.

--- classes/LevelUpMessage.js
+++ classes/LevelUpMessage.js
@@ -63,35 +63,3 @@ class LevelUpMessage {
                     // go one extra layer deep lmao
                     else if (val && typeof val == "object" && !Array.isArray(val)) {
                         for (const [key2, val2] of Object.entries(val)) {
-                            if (typeof val2 == "string") mbedJSON[key][key2] = this.subVariables(val2)
-                        }
-                    }
-                }
-
-                // add vars to fields
-                    if (mbedJSON.fields && mbedJSON.fields.length) {
-                    mbedJSON.fields = mbedJSON.fields.map(f => ({ name: this.subVariables(f.name), value: this.subVariables(f.value), inline: f.inline }))
-                }
-                
-                this.msg = { embeds: [ mbedJSON ] }
-                if (mbed.extraContent) this.msg.content = this.subVariables(mbed.extraContent)
-            }
-        }
-
-        else this.msg =  { content: this.subVariables(this.msg) }
-
-        if (this.msg) this.msg.reply = { messageReference: message.id }
-    }
-
-    subVariables(msg) {
-
-        if (!msg) return msg
-        let newMsg = msg.replace(/\n/g, "　")
-        let newLevel = this.level
-
-        // simple variables
-        let vars = this.variables        
-        newMsg = newMsg.replace(/\[\[[A-Z@_ ]+\]\]/g, function(str) {
-            let v = str.slice(2, -2).trim()
-            return vars[v] ?? str
-        })
